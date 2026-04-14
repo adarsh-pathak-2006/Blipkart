b954dac9c7c746378786b97a8a6394791b3026af
duckyduck01 <madhvetipathak2006@gmail.com>
2026-04-14 12:57:29 +0530

    bugs fixed

--- blipkart/views.py
+++ blipkart/views.py
@@ -4,7 +4,7 @@ def home(request):
     return render(request, 'home.html')
 
 def cart(request):
-    return render(request, 'about.html')
+    return render(request, 'cart.html')
 
 def account(request):
     return render(request, 'account.html')
